a73b1e95e2c229d4ae8acd51ad4b39e1c46f8584
Lutz Lengemann <lutz@lengemann.net>
2024-09-16 15:06:16 +0200

    Changes

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,10 +1,10 @@
 # Use `hub` as our git wrapper:
 #   https://hub.github.com/
-hub_path=$(which hub)
-if (( $+commands[hub] ))
-then
-  alias git=$hub_path
-fi
+# hub_path=$(which hub)
+# if (( $+commands[hub] ))
+# then
+#   alias git=$hub_path
+# fi
 
 # The rest of my fun git aliases
 alias gl='git pull --prune'
