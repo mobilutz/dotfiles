1b4785b501a2e88a1f53dc1eb67e08243539e02b
Lutz Lengemann <lutz@lengemann.net>
2025-04-01 11:34:47 +0200

    Changes

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -274,6 +274,9 @@ defaults write com.apple.screencapture type -string "jpg"
 # Disable shadow in screenshots
 defaults write com.apple.screencapture disable-shadow -bool true
 
+# Disable screenshot preview
+defaults write com.apple.screencapture show-thumbnail -bool false
+
 # Enable subpixel font rendering on non-Apple LCDs
 # Reference: https://github.com/kevinSuttle/macOS-Defaults/issues/17#issuecomment-266633501
 defaults write NSGlobalDomain AppleFontSmoothing -int 1
